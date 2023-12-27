f3a77a573282ab8cd0853793a6148a361a0c9710
Sbrjt <champa.281982@gmail.com>
2023-12-27 23:14:53 +0530

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const button = document.getElementById('button')
-const table = document.getElementById('table')
+const btn = document.getElementById('btn')
+const btn2 = document.getElementById('btn2')
 
 async function loadDb() {
 	const response = await fetch('data.db')
@@ -7,11 +7,14 @@ async function loadDb() {
 	db = new SQL.Database(new Uint8Array(buffer)) // global var db
 }
 
-async function showTable() {
+async function fetchData() {
 	// variables
+	const tb = document.getElementById('tb')
+	const table1 = document.getElementById('table1')
+	const table2 = document.getElementById('table2')
 	const rank = document.getElementById('rank').value
 	const category = document.getElementById('category').value
-	const state = document.getElementById('state').value
+	// const state = document.getElementById('state').value
 	const gender = neutral.checked ? 'Neutral' : 'Female'
 
 	let branch = ''
@@ -19,14 +22,13 @@ async function showTable() {
 		branch += `'${i.value}', `
 	}
 	branch = branch.slice(0, -2)
-	console.log(branch)
 
-	const college = []
-	for (const i of document.getElementsByName('college')) {
-		if (i.checked) {
-			college.push(i.value)
-		}
-	}
+	// const college = []
+	// for (const i of document.getElementsByName('college')) {
+	// 	if (i.checked) {
+	// 		college.push(i.value)
+	// 	}
+	// }
 
 	// sql query
 	const query = `select * from data
@@ -35,22 +37,41 @@ async function showTable() {
     closing > ${rank} and
 	branch in (${branch})
     order by closing
-	limit 20`
+	limit 50`
 
 	// fetched data in 2D array
 	const result = db.exec(query)[0].values
 
-	// clear table
-	table.innerHTML = ''
+	// show table
+	tb.classList.remove('d-none')
 
-	// iterating through result and inserting cells to table
-	for (let i = 0; i < result.length; i++) {
-		let row = table.insertRow(i)
+	// clear table
+	table1.innerHTML = ''
+
+	// iterating through result and inserting cells to table1, table2
+
+	for (let i = 0; i < 10; i++) {
+		let row = table1.insertRow(i)
+		for (let j = 0; j < 7; j++) {
+			row.insertCell(j).innerHTML = result[i][j]
+		}
+	}
+	for (let i = 10; i < result.length; i++) {
+		let row = table2.insertRow(i - 10)
 		for (let j = 0; j < 7; j++) {
 			row.insertCell(j).innerHTML = result[i][j]
 		}
 	}
 }
 
-button.addEventListener('click', showTable)
+function expand() {
+	if (btn2.innerHTML == 'Expand') {
+		btn2.innerHTML = 'Collapse'
+	} else {
+		btn2.innerHTML = 'Expand'
+	}
+}
+
+btn.addEventListener('click', fetchData)
+btn2.addEventListener('click', expand)
 document.addEventListener('DOMContentLoaded', loadDb)
