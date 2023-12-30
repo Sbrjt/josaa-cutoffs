b1dc22f0ee3be4fe7e02253aad5ffd3d61b46fc2
Sbrjt <champa.281982@gmail.com>
2023-12-30 20:02:30 +0530

    ...

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const btn = document.getElementById('btn')
-const btn2 = document.getElementById('btn2')
+const btn = document.getElementById('btn') // GO btn
+const btn2 = document.getElementById('btn2') // 'Show more' btn
+let x // row no
+let db // database for loadDb
+let result
 
 async function loadDb() {
 	const response = await fetch('data.db')
@@ -8,69 +11,84 @@ async function loadDb() {
 }
 
 function fetchData() {
-	// variables
-	const tb = document.getElementById('tb')
-	const table1 = document.getElementById('table1')
-	const table2 = document.getElementById('table2')
-	const rank = document.getElementById('rank').value
-	const category = document.getElementById('category').value
-	// const state = document.getElementById('state').value
-	const gender = neutral.checked ? 'Neutral' : 'Female'
+	// variables:
 
-	let branch = ''
-	for (const i of document.getElementById('branch').selectedOptions) {
-		branch += `'${i.value}', `
+	let rank = document.getElementById('rank').value
+	if (rank === '') {
+		rank = 1 // default value
 	}
-	branch = branch.slice(0, -2)
 
-	// const college = []
-	// for (const i of document.getElementsByName('college')) {
-	// 	if (i.checked) {
-	// 		college.push(i.value)
-	// 	}
-	// }
+	const category = document.getElementById('category').value
 
-	// sql query
-	const query = `select * from data
-	where seat = '${category}' and
-	gender = '${gender}' and
-    closing > ${rank} and
-	branch in (${branch})
-    order by closing
-	limit 50`
+	// Getting selected values from 'branch' and join them (with commas and quotes) to prepare SQL query
+	const branch = Array.from(document.getElementById('branch').selectedOptions)
+		.map(i => `'${i.value}'`)
+		.join(', ')
 
-	// fetched data in 2D array
-	const result = db.exec(query)[0].values
+	const state = document.getElementById('state').value
 
-	// show table
+	const gender = document.getElementsByName('gender')[0].checked
+		? 'Neutral'
+		: 'Female'
+
+	// get elements with name 'type', convert them to array, filter to get only the checked ones,
+	// map them to an array of their values wrapped in single quotes,
+	// join the array into a comma-separated string to prepare SQL query
+	const type = Array.from(document.getElementsByName('type'))
+		.filter(i => i.checked)
+		.map(i => `'${i.value}'`)
+		.join(', ')
+
+	const tb = document.getElementById('tb') //parent table
+	const table = document.getElementById('table') //table body
+
+	// show table (as it is hidden initially)
 	if (tb.classList.contains('d-none')) {
 		tb.classList.remove('d-none')
 	}
 
-	// clear table
-	table1.innerHTML = ''
+	// clear table contents
+	table.innerHTML = ''
 
-	// iterating through result and inserting cells to table1, table2
+	// sql query
+	const query = `
+	select  Institute, Branch, Quota, Seat_type, Gender, Opening_rank, Closing_rank from tb
+	where Seat_type = '${category}' and
+	Gender = '${gender}' and
+    Closing_rank >= ${rank} and
+	Branch in (${branch}) and
+	(
+        (Quota in ('HS', 'GO', 'LA', 'JK') and State = '${state}')
+        or (Quota = 'OS' and State != '${state}')
+        or Quota = 'AI' or
+		'${state}' = 'Select an option'
+    ) and
+	Institute_type in (${type})
+    order by Closing_rank
+	`
 
-	for (let i = 0; i < 10; i++) {
-		let row = table1.insertRow(i)
-		for (let j = 0; j < 7; j++) {
-			row.insertCell(j).innerHTML = result[i][j]
+	// fetched data in 2D array
+	result = db.exec(query)[0].values
+
+	x = 0
+	// iterating through result and inserting 10 rows into table (x - row, y - col)
+	for (let i = 0; i < 10 && x < result.length; i++) {
+		let row = table.insertRow()
+		for (let y = 0; y < 7; y++) {
+			row.insertCell(y).innerHTML = result[x][y]
 		}
-	}
-	for (let i = 10; i < result.length; i++) {
-		let row = table2.insertRow(i - 10)
-		for (let j = 0; j < 7; j++) {
-			row.insertCell(j).innerHTML = result[i][j]
-		}
+		x++
 	}
 }
 
 function expand() {
-	if (btn2.innerHTML == 'Expand') {
-		btn2.innerHTML = 'Collapse'
-	} else {
-		btn2.innerHTML = 'Expand'
+	// insert 10 more records
+	for (let i = 0; i < 10 && x < result.length; i++) {
+		let row = table.insertRow()
+		for (let y = 0; y < 7; y++) {
+			row.insertCell(y).innerHTML = result[x][y]
+		}
+		x++
 	}
 }
 
